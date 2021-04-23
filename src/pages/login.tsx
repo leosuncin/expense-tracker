406e6176a555b53e95d80f4081b92a384882583d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-22 21:01:13 -0600

    feat(auth): add login and register pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import LoginForm from '@app/features/auth/LoginForm';
+
+const LoginPage: NextPage = () => {
+  return <LoginForm />;
+};
+
+export default LoginPage;
---
+++ src/pages/register.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import RegisterForm from '@app/features/auth/RegisterForm';
+
+const RegisterPage: NextPage = () => {
+  return <RegisterForm />;
+};
+
+export default RegisterPage;
