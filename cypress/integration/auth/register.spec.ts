40fc7c537430e02eddf0254d6de7c44e064726a1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-24 02:43:58 -0600

    test(auth): e2e test the register page

---
+++ cypress/integration/auth/register.spec.ts
@@ -0,0 +1,51 @@
+import { registerFactory } from '@app/features/auth/authFactories';
+
+describe('Register page', () => {
+  beforeEach(() => {
+    cy.task('loadFixtures');
+
+    cy.intercept('POST', '**/api/auth/register').as('register');
+    cy.visit('/register');
+  });
+
+  it('shows the validation errors', () => {
+    cy.findByRole('button', { name: /register/i }).click();
+
+    cy.findByText('Name is required').should('be.visible');
+    cy.findByText('Email is required').should('be.visible');
+    cy.findByText('Password is required').should('be.visible');
+
+    cy.findByLabelText(/name/i).type('a');
+    cy.findByLabelText(/email/i).type('email');
+    cy.findByLabelText(/password/i).type('123456');
+
+    cy.findByText('Name has to be at least 2 letters').should('be.visible');
+    cy.findByText('Email needs to be a valid address').should('be.visible');
+    cy.findByText('Password has to be at least 12 characters').should(
+      'be.visible',
+    );
+  });
+
+  it('creates a new user', () => {
+    const data = registerFactory.build();
+
+    cy.findByLabelText(/name/i).type(data.name);
+    cy.findByLabelText(/email/i).type(data.email);
+    cy.findByLabelText(/password/i).type(data.password);
+    cy.findByRole('button', { name: /register/i }).click();
+
+    cy.wait('@register').its('response.statusCode').should('equal', 201);
+  });
+
+  it('fails to register a duplicate user', () => {
+    const data = registerFactory.build({ email: 'john@doe.me' });
+
+    cy.findByLabelText(/name/i).type(data.name);
+    cy.findByLabelText(/email/i).type(data.email);
+    cy.findByLabelText(/password/i).type(data.password);
+    cy.findByRole('button', { name: /register/i }).click();
+
+    cy.wait('@register').its('response.statusCode').should('equal', 409);
+    cy.findByText('Duplicate data: is already register').should('be.visible');
+  });
+});
