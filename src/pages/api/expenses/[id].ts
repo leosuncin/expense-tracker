b93fc9d6566198731508ecc4993edfe86b7a08a4
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-24 22:44:09 -0600

    refactor(expenses): move verify logic to its own middleware
    Reduce duplicate code.

--- src/pages/api/expenses/[id].ts
+++ src/pages/api/expenses/[id].ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import connect from 'next-connect';
+import connect, { NextHandler } from 'next-connect';
 
 import type { User } from '@app/features/auth/User';
 import { Expense } from '@app/features/expenses/Expense';
@@ -18,34 +18,8 @@ import {
 
 const getExpenseHandler: ApiHandler = async (request, response) => {
   const expenseId = request.query.id as string;
-  const author = request.session.get<User>('user');
-
-  if (!author) {
-    response.status(StatusCodes.FORBIDDEN).json({
-      message: 'You must be logged in order to access',
-      statusCode: StatusCodes.FORBIDDEN,
-    });
-    return;
-  }
-
-  const expense = await Expense.findById(expenseId);
-
-  if (!expense) {
-    response.status(StatusCodes.NOT_FOUND).json({
-      message: `There is not any expense with id: ${expenseId}`,
-      statusCode: StatusCodes.NOT_FOUND,
-    });
-    return;
-  }
-
-  // @ts-expect-error
-  if (expense.author.toHexString() !== author._id && !author.isAdmin) {
-    response.status(StatusCodes.FORBIDDEN).json({
-      message: `You need to be the author of the expense in order to access it`,
-      statusCode: StatusCodes.FORBIDDEN,
-    });
-    return;
-  }
+  // @ts-expect-error it exists, is checked in the middleware
+  const expense: Expense = await Expense.findById(expenseId);
 
   response.json(expense.toJSON());
 };
@@ -55,34 +29,8 @@ const updateExpenseHandler: ApiHandler<UpdateExpense> = async (
   response,
 ) => {
   const expenseId = request.query.id as string;
-  const author = request.session.get<User>('user');
-
-  if (!author) {
-    response.status(StatusCodes.FORBIDDEN).json({
-      message: 'You must be logged in order to access',
-      statusCode: StatusCodes.FORBIDDEN,
-    });
-    return;
-  }
-
-  const expense = await Expense.findById(expenseId);
-
-  if (!expense) {
-    response.status(StatusCodes.NOT_FOUND).json({
-      message: `There is not any expense with id: ${expenseId}`,
-      statusCode: StatusCodes.NOT_FOUND,
-    });
-    return;
-  }
-
-  // @ts-expect-error
-  if (expense.author.toHexString() !== author._id && !author.isAdmin) {
-    response.status(StatusCodes.FORBIDDEN).json({
-      message: `You need to be the author of the expense in order to access it`,
-      statusCode: StatusCodes.FORBIDDEN,
-    });
-    return;
-  }
+  // @ts-expect-error it exists, is checked in the middleware
+  const expense: Expense = await Expense.findById(expenseId);
 
   expense.name = request.body.name ?? expense.name;
   expense.amount = request.body.amount ?? expense.amount;
@@ -94,6 +42,20 @@ const updateExpenseHandler: ApiHandler<UpdateExpense> = async (
 
 const removeExpenseHandler: ApiHandler = async (request, response) => {
   const expenseId = request.query.id as string;
+  // @ts-expect-error it exists, is checked in the middleware
+  const expense: Expense = await Expense.findById(expenseId);
+
+  await expense.remove();
+
+  response.status(StatusCodes.NO_CONTENT).send(Buffer.alloc(0));
+};
+
+async function verifyConditionsMiddleware(
+  request: Parameters<ApiHandler>[0],
+  response: Parameters<ApiHandler>[1],
+  next: NextHandler,
+) {
+  const expenseId = request.query.id as string;
   const author = request.session.get<User>('user');
 
   if (!author) {
@@ -114,7 +76,7 @@ const removeExpenseHandler: ApiHandler = async (request, response) => {
     return;
   }
 
-  // @ts-expect-error
+  // @ts-expect-error `author` is not populated
   if (expense.author.toHexString() !== author._id && !author.isAdmin) {
     response.status(StatusCodes.FORBIDDEN).json({
       message: `You need to be the author of the expense in order to access it`,
@@ -123,15 +85,14 @@ const removeExpenseHandler: ApiHandler = async (request, response) => {
     return;
   }
 
-  await expense.remove();
-
-  response.status(StatusCodes.NO_CONTENT).send(Buffer.alloc(0));
-};
+  next();
+}
 
 export default connect({ onError: errorMiddleware })
   .use(databaseMiddleware)
   .use(sessionMiddleware)
   .use(castObjectIdMiddleware)
+  .use(verifyConditionsMiddleware)
   .use(validationMiddleware(updateExpenseSchema))
   .get(getExpenseHandler)
   .put(updateExpenseHandler)
