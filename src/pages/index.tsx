832d50eade0dc958b96a6215bf98f55dcddd27f5
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-26 19:15:23 -0600

    feat(expenses): show expenses in the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,27 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import { useEffect } from 'react';
 
-import Counter from '@app/features/counter/Counter';
-import styles from '@app/styles/Home.module.css';
+import { useAppDispatch } from '@app/app/hooks';
+import AddExpense from '@app/features/expenses/AddExpense';
+import { findExpenses } from '@app/features/expenses/expenseSlice';
+import ExpensesTable from '@app/features/expenses/ExpensesTable';
 
 const IndexPage: NextPage = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const promise = dispatch(findExpenses());
+
+    return () => {
+      promise.abort();
+    };
+  }, [dispatch]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Redux Toolkit</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className={styles.header}>
-        <img src="/logo.svg" className={styles.logo} alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <AddExpense />
+      <ExpensesTable />
+    </>
   );
 };
 
