549d7ecaf6393220cdaed525dacb1d98679d0006
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-28 23:02:41 -0600

    feat: setup next-redux-wrapper
    Initialiaze router state from context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,15 @@ import '@app/styles/globals.css';
 
 import { ConnectedRouter } from 'connected-next-router';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
 
-import store from '@app/app/store';
+import { wrapper } from '@app/app/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ConnectedRouter>
-        <Component {...pageProps} />
-      </ConnectedRouter>
-    </Provider>
+    <ConnectedRouter>
+      <Component {...pageProps} />
+    </ConnectedRouter>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
