a583171c4eadde3d347a02fed162404a185dec52
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-05-24 15:40:11 -0600

    style(auth): extract api handlers from endpoints

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,41 +1,13 @@
-import { StatusCodes } from 'http-status-codes';
 import connect from 'next-connect';
 
-import { LoginUser, loginSchema } from '@app/features/auth/authSchemas';
-import User, { UserJson } from '@app/features/auth/User';
+import { loginHandler } from '@app/features/auth/authApiHandlers';
+import { loginSchema } from '@app/features/auth/authSchemas';
 import {
-  ApiHandler,
   databaseMiddleware,
   errorMiddleware,
   sessionMiddleware,
   validationMiddleware,
 } from '@app/utils/middleware';
-
-const loginHandler: ApiHandler<LoginUser> = async (request, response) => {
-  const { email, password } = request.body;
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    response.status(StatusCodes.UNAUTHORIZED).json({
-      message: `There isn't any user with email: ${email}`,
-      statusCode: StatusCodes.UNAUTHORIZED,
-    });
-    return;
-  }
-
-  if (!(await user.checkPassword(password))) {
-    response.status(StatusCodes.UNAUTHORIZED).json({
-      message: `Wrong password for user with email: ${email}`,
-      statusCode: StatusCodes.UNAUTHORIZED,
-    });
-    return;
-  }
-
-  request.session.set('user', user.toJSON<UserJson>());
-  await request.session.save();
-
-  response.json(user.toJSON<UserJson>());
-};
 
 export default connect({ onError: errorMiddleware })
   .use(databaseMiddleware)
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,25 +1,13 @@
-import { StatusCodes } from 'http-status-codes';
 import connect from 'next-connect';
 
-import { RegisterUser, registerSchema } from '@app/features/auth/authSchemas';
-import User, { UserJson } from '@app/features/auth/User';
+import { registerHandler } from '@app/features/auth/authApiHandlers';
+import { registerSchema } from '@app/features/auth/authSchemas';
 import {
-  ApiHandler,
   databaseMiddleware,
   errorMiddleware,
   sessionMiddleware,
   validationMiddleware,
 } from '@app/utils/middleware';
-
-const registerHandler: ApiHandler<RegisterUser> = async (request, response) => {
-  const user = new User(request.body);
-
-  await user.save();
-  request.session.set('user', user.toJSON<UserJson>());
-  await request.session.save();
-
-  response.status(StatusCodes.CREATED).json(user.toJSON<UserJson>());
-};
 
 export default connect({ onError: errorMiddleware })
   .use(databaseMiddleware)
